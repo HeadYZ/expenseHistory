63bb05273f6b02a9c83d612dd56aede9c7041564
Patryk <krasowski.p@outlook.com>
2023-10-07 22:11:21 +0200

    created dynamic filtred list

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,9 @@
 import './ExpenseItem.css'
 import Card from '../UI/Card'
-import { useState } from 'react'
 export default function ExpenseItem(expenses) {
 	const month = expenses.date.toLocaleString('en-US', { month: 'long' })
 	const day = expenses.date.toLocaleString('en-US', { day: '2-digit' })
 	const year = expenses.date.getFullYear()
-	const [title, setTitle] = useState(expenses.title)
-
-	const setTitleHandler = () => {
-		setTitle('dziala')
-	}
 
 	return (
 		<Card className='expense-item'>
@@ -19,10 +13,9 @@ export default function ExpenseItem(expenses) {
 				<p className='expense-date__year'>{year}</p>
 			</div>
 			<div className='expense-item__description'>
-				<h2>{title}</h2>
+				<h2>{expenses.title}</h2>
 				<p className='expense-item__price'>${expenses.amount}</p>
 			</div>
-			<button onClick={setTitleHandler}>Cos do klikniecia</button>
 		</Card>
 	)
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,21 +2,24 @@ import ExpenseItem from './ExpenseItem'
 import Card from '../UI/Card'
 import ExpenseFilter from './ExpensesFilter.js'
 import './Expenses.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Expenses(props) {
-	const [filtedExpenses, setFiltredExpenses] = useState(props.expenses)
+	const [filtredExpenses, setFiltredExpenses] = useState([])
 
 	const filteredExpensesHandler = selectedDate => {
 		const filtredExpenses = props.expenses.filter(expense => expense.date.getFullYear().toString() === selectedDate)
 
 		setFiltredExpenses(filtredExpenses)
 	}
+	useEffect(() => {
+		filteredExpensesHandler('2023')
+	}, [props.expenses])
 
 	return (
 		<Card className='expenses'>
 			<ExpenseFilter onChangeDate={filteredExpensesHandler}></ExpenseFilter>
-			{filtedExpenses.map(expense => (
+			{filtredExpenses.map(expense => (
 				<ExpenseItem date={expense.date} title={expense.title} amount={expense.amount} key={expense.id}></ExpenseItem>
 			))}
 		</Card>
